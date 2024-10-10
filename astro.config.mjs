fe6c4d754dde8000f2eea19a803bea62bc710c92
redaezziani <klausdev2@gmail.com>
2024-10-10 20:52:50 +0100

    Refactor code and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeSlug from 'rehype-slug';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://www.kozhuhds.com',
+  integrations: [
+    tailwind(), // Integrate Tailwind CSS
+    mdx({
+      syntaxHighlight: false, // Disable default syntax highlighting (if using rehypePrettyCode)
+      rehypePlugins: [
+        rehypeSlug, // Adds ids to headings for linking
+        [
+          rehypePrettyCode, // Enhances code blocks with syntax highlighting
+          {
+            theme: 'github-dark', // Set the syntax highlighting theme
+          },
+        ],
+      ],
+    }),
+  ],
+});
